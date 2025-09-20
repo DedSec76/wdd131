4b860fe3f7cd5ac2edbaf72f19fe3d9c47dd8f01
Aldair Rutte <xxpiltisxx@gmail.com>
2025-09-20 17:46:05 -0500

    Finished place.html with its css and js

--- scripts/place.js
+++ scripts/place.js
@@ -9,20 +9,17 @@ function calculateWindChill(t, v) {
     return 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
 }
 
-// Datos de ejemplo
+// Static Data
 const temp = 10;   // °C
 const wind = 5;  // km/h
 
-// Solo invocar si se cumplen las condiciones oficiales
-let mensaje = "No aplica sensación térmica.";
+// Only calculate if the conditions are met
+let menssage = "Not Applicable";
 if (temp <= 10 && wind > 4.8) {
     const wc = calculateWindChill(temp, wind);
-    mensaje = `${wc.toFixed(1)} °C`;
-}
-else {
-    mensaje = "N/A";
+    menssage = `${wc.toFixed(1)} °C`;
 }
 
-document.getElementById("windChill").innerText = mensaje;
+document.getElementById("windChill").innerText = menssage;
 
 
