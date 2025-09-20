947288d17bb44e31a844764265502e1de8ffb756
Aldair Rutte <xxpiltisxx@gmail.com>
2025-09-20 00:39:07 -0500

    Added place.html with its styles

---
+++ scripts/place.js
@@ -0,0 +1,5 @@
+
+
+const date = new Date();
+document.getElementById("currentyear").textContent = date.getFullYear();
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
